cdde56eb8ab37ee65d97f94dc2b2a9e2a406e353
cai584770 <www.1614477920@qq.com>
2019-09-12 11:31:28 +0800

    Create for.cpp
    C语言数瓶子童谣-->for版本

---
+++ C/for.cpp
@@ -0,0 +1,41 @@
+//for
+#include <stdio.h>
+void put1(int a, int b);
+void put2();
+
+int main()
+{
+	put1(99, 0);
+
+	return 0;
+}
+
+void put1(int a, int b)
+{
+	a = 99;
+
+	for	(b = 0;b < a;a--)
+	{
+		if (a > 1)
+		{
+			printf("%d bottles of beer on the wall. %d bottles of beer.\n", a, a);
+			put2();
+			if (a > 2)
+				printf("%d bottles of beer on the wall.\n", a - 1);
+			else
+				printf("%d bottle of beer on the wall.\n", a - 1);
+		}
+		else
+		{
+			printf("%d bottle of beer on the wall. %d bottle of beer.\n", a, a);
+			put2();
+			printf("No more bottles on the wall.");
+		}
+	}
+}
+
+void put2()
+{
+	printf("Take one down.\n");
+	printf("Pass it around.\n");
+}
